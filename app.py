5998ecf13245d7775dcd99c9c6675d9cb637e7d1
gdevisa <31815792+gdevisa@users.noreply.github.com>
2020-09-18 18:07:13 -0700

    Layout Done

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 
 from collections import Counter
 import emoji
@@ -18,11 +19,7 @@ import plotly.graph_objects as go
 import pandas as pd
 import json
 
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-from skimage import io
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.LITERA]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -33,26 +30,28 @@ colors = {
 }
 
 app.layout = html.Div([
-    dcc.Upload(
-            id='upload-file',
-            children=html.Div([
-                'Drag and Drop or ',
-                html.A('Select Files')
-            ]),
-            style={
-                'width': '100%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin': '10px'
-            },
-            # Allow multiple files to be uploaded
-            multiple=False
-        ),
-    html.Div(id='output-file-upload')
+    dbc.Container([
+        dcc.Upload(
+                id='upload-file',
+                children=html.Div([
+                    'Drag and Drop or ',
+                    html.A('Select Files')
+                ]),
+                style={
+                    'width': '100%',
+                    'height': '60px',
+                    'lineHeight': '60px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'textAlign': 'center',
+                    'margin': '10px'
+                },
+                # Allow multiple files to be uploaded
+                multiple=False
+            ),
+        html.Div(id='output-file-upload')
+    ])
 ])
 
 
@@ -66,25 +65,28 @@ def update_output(contents, filename):
         emoji_dict = dict(Counter(total_emojis_list))
         emoji_dict = sorted(emoji_dict.items(), key=lambda x: x[1], reverse=True)
         emoji_df = pd.DataFrame(emoji_dict, columns=['emoji', 'count'])
+        emoji_df = emoji_df[:8]
 
         labels = df['from'].unique()
         values = df.groupby(by='from', axis=0).size()
-        fig_pie = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]],
-                                subplot_titles=('Total Messages', 'Total Emojis'))
-        fig_pie.add_trace(go.Pie(labels=labels, values=values,
-                                 textinfo='label+value+percent'), row=1, col=1)
-        fig_pie.add_trace(go.Pie(labels=emoji_df.emoji[:8], values=emoji_df['count'][:8],
-                                 textinfo='label+percent'), row=1, col=2)
-        fig_pie.update_layout(showlegend=False)
 
+        fig_pie = px.pie(emoji_df, names='emoji', values='count')
+        fig_pie.update_traces(textposition='inside', textinfo='percent+label', showlegend=False)
+        fig_pie.update_layout(
+            title={
+                'text': 'Emojis Used',
+                'y': 0.92,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'})
 
-        #MessagesHistory line chart
-        date_df = df.groupby('Date').size().rename_axis('Date').to_frame('MessageCount')
+        #Chronology line chart
+        date_df = df.groupby('Date').size().rename_axis('Date').to_frame('Messages')
         date_df.reset_index(inplace=True)
-        fig_line = px.line(date_df, x='Date', y='MessageCount')
+        fig_line = px.line(date_df, x='Date', y='Messages')
         fig_line.update_layout(
             title={
-                'text': "Message History",
+                'text': "Chronology",
                 'y':0.92,
                 'x':0.5,
                 'xanchor': 'center',
@@ -108,88 +110,85 @@ def update_output(contents, filename):
                     mode='lines', fill='tozeroy'), 1, 2)
         fig.update_layout(showlegend=False)
 
-        #Generating a wordcloud
-        texts = df['text']
-        #exec(open("stopwords.txt").read(), globals())
-        stopwords = ["типа", "просто", "вроде", "ну", "вообще", "там", "короче", "потом", "раз", "тоже", "че", "давай",
-                     "тут", "сейчас", "бля", "надо", "шо", "много", "сегодня", "завтра", "вчера", "понял", "очень",
-                     "хочу", "думаю", "кстати", "ща", "знаю", "Оо", "Ооо", "Окей", "наверное", "думал", "пару", "чисто",
-                     "думаю", "бо", "хочу", "хотел", "какие", "понимаю", "норм", "мб", "c", "а", "алло", "без", "белый",
-                     "близко", "более", "больше", "большой", "будем", "будет", "будете", "будешь", "будто", "буду",
-                     "будут", "будь", "бы", "бывает", "бывь", "был", "была", "были", "было", "быть", "в", "важная",
-                     "важное", "важные", "важный", "вам", "вами", "вас", "ваш", "ваша", "ваше", "ваши", "вверх",
-                     "вдали", "вдруг", "ведь", "везде", "вернуться", "весь", "вечер", "взгляд", "взять", "вид", "видел",
-                     "видеть", "вместе", "вне", "вниз", "внизу", "во", "вода", "война", "вокруг", "вон", "вообще",
-                     "вопрос", "восемнадцатый", "восемнадцать", "восемь", "восьмой", "вот", "впрочем", "времени",
-                     "время", "все", "все еще", "всегда", "всего", "всем", "всеми", "всему", "всех", "всею", "всю",
-                     "всюду", "вся", "всё", "второй", "вы", "выйти", "г", "где", "главный", "глаз", "говорил",
-                     "говорит", "говорить", "год", "года", "году", "голова", "голос", "город", "да", "давать", "давно",
-                     "даже", "далекий", "далеко", "дальше", "даром", "дать", "два", "двадцатый", "двадцать", "две",
-                     "двенадцатый", "двенадцать", "дверь", "двух", "девятнадцатый", "девятнадцать", "девятый", "девять",
-                     "действительно", "дел", "делал", "делать", "делаю", "дело", "день", "деньги", "десятый", "десять",
-                     "для", "до", "довольно", "долго", "должен", "должно", "должный", "дом", "дорога", "друг", "другая",
-                     "другие", "других", "друго", "другое", "другой", "думать", "душа", "е", "его", "ее", "ей", "ему",
-                     "если", "есть", "еще", "ещё", "ею", "её", "ж", "ждать", "же", "жена", "женщина", "жизнь", "жить",
-                     "за", "занят", "занята", "занято", "заняты", "затем", "зато", "зачем", "здесь", "земля", "знать",
-                     "значит", "значить", "и", "иди", "идти", "из", "или", "им", "имеет", "имел", "именно", "иметь",
-                     "ими", "имя", "иногда", "их", "к", "каждая", "каждое", "каждые", "каждый", "кажется", "казаться",
-                     "как", "какая", "какой", "кем", "книга", "когда", "кого", "ком", "комната", "кому", "конец",
-                     "конечно", "которая", "которого", "которой", "которые", "который", "которых", "кроме", "кругом",
-                     "кто", "куда", "лежать", "лет", "ли", "лицо", "лишь", "лучше", "любить", "люди", "м", "маленький",
-                     "мало", "мать", "машина", "между", "меля", "менее", "меньше", "меня", "место", "миллионов", "мимо",
-                     "минута", "мир", "мира", "мне", "много", "многочисленная", "многочисленное", "многочисленные",
-                     "многочисленный", "мной", "мною", "мог", "могу", "могут", "мож", "может", "может быть", "можно",
-                     "можхо", "мои", "мой", "мор", "москва", "мочь", "моя", "моё", "мы", "на", "наверху", "над", "надо",
-                     "назад", "наиболее", "найти", "наконец", "нам", "нами", "народ", "нас", "начала", "начать", "наш",
-                     "наша", "наше", "наши", "не", "него", "недавно", "недалеко", "нее", "ней", "некоторый", "нельзя",
-                     "нем", "немного", "нему", "непрерывно", "нередко", "несколько", "нет", "нею", "неё", "ни",
-                     "нибудь", "ниже", "низко", "никакой", "никогда", "никто", "никуда", "ним", "ними", "них", "ничего",
-                     "ничто", "но", "новый", "нога", "ночь", "ну", "нужно", "нужный", "нх", "о", "об", "оба", "обычно",
-                     "один", "одиннадцатый", "одиннадцать", "однажды", "однако", "одного", "одной", "оказаться", "окно",
-                     "около", "он", "она", "они", "оно", "опять", "особенно", "остаться", "от", "ответить", "отец",
-                     "откуда", "отовсюду", "отсюда", "очень", "первый", "перед", "писать", "плечо", "по", "под",
-                     "подойди", "подумать", "пожалуйста", "позже", "пойти", "пока", "пол", "получить", "помнить",
-                     "понимать", "понять", "пор", "пора", "после", "последний", "посмотреть", "посреди", "потом",
-                     "потому", "почему", "почти", "правда", "прекрасно", "при", "про", "просто", "против", "процентов",
-                     "путь", "пятнадцатый", "пятнадцать", "пятый", "пять", "работа", "работать", "раз", "разве", "рано",
-                     "раньше", "ребенок", "решить", "россия", "рука", "русский", "ряд", "рядом", "с", "с кем", "сам",
-                     "сама", "сами", "самим", "самими", "самих", "само", "самого", "самой", "самом", "самому", "саму",
-                     "самый", "свет", "свое", "своего", "своей", "свои", "своих", "свой", "свою", "сделать", "сеаой",
-                     "себе", "себя", "сегодня", "седьмой", "сейчас", "семнадцатый", "семнадцать", "семь", "сидеть",
-                     "сила", "сих", "сказал", "сказала", "сказать", "сколько", "слишком", "слово", "случай", "смотреть",
-                     "сначала", "снова", "со", "собой", "собою", "советский", "совсем", "спасибо", "спросить", "сразу",
-                     "стал", "старый", "стать", "стол", "сторона", "стоять", "страна", "суть", "считать", "т", "та",
-                     "так", "такая", "также", "таки", "такие", "такое", "такой", "там", "твои", "твой", "твоя", "твоё",
-                     "те", "тебе", "тебя", "тем", "теми", "теперь", "тех", "то", "тобой", "тобою", "товарищ", "тогда",
-                     "того", "тоже", "только", "том", "тому", "тот", "тою", "третий", "три", "тринадцатый",
-                     "тринадцать", "ту", "туда", "тут", "ты", "тысяч", "у", "увидеть", "уж", "уже", "улица", "уметь",
-                     "утро", "хороший", "хорошо", "хотел бы", "хотеть", "хоть", "хотя", "хочешь", "час", "часто",
-                     "часть", "чаще", "чего", "человек", "чем", "чему", "через", "четвертый", "четыре", "четырнадцатый",
-                     "четырнадцать", "что", "чтоб", "чтобы", "чуть", "шестнадцатый", "шестнадцать", "шестой", "шесть",
-                     "эта", "эти", "этим", "этими", "этих", "это", "этого", "этой", "этом", "этому", "этот", "эту", "я",
-                     "являюсь"]
-        stopwords = set(stopwords)
-        wordcloud = WordCloud(stopwords=stopwords,
-                              width=1000, height=500, random_state=1, background_color='white', colormap='Set2',
-                              collocations=False,
-                              ).generate(' '.join(texts))
-        plt.imshow(wordcloud)  # image show
-        plt.axis('off')  # to off the axis of x and y
-        plt.savefig('Plotly-World_Cloud.png')
+        #Total Stats
+        jumbotron = dbc.Jumbotron(
+            [
+                html.H1("Results", className="display-3"),
+                html.Hr(className="my-2"),
+                html.P(),
+                html.H5('Total Messages', className="card-title"),
+                html.P(
+                    str(len(df)),
+                    className="card-text",
+                ),
+                html.H5('Total Words', className="card-title"),
+                html.P(
+                    str(len(df)),
+                    className="card-text",
+                ),
+                html.H5('Time Period', className="card-title"),
+                html.P(
+                    'from {0} to {1}'.format(str(df.Date[0]), str(df.Date[len(df)])),
+                    className="card-text",
+                ),
 
-        img = io.imread(
-            'Plotly-World_Cloud.png')
-        fig_wordcloud = px.imshow(img)
-        fig_wordcloud.update_layout(coloraxis_showscale=False)
-        fig_wordcloud.update_xaxes(showticklabels=False)
-        fig_wordcloud.update_yaxes(showticklabels=False)
+            ], style={'textAlign': 'center'}
+        )
 
-        return [html.H1('Total Messages: ' + str(len(df)), style={'textAlign': 'center'}),
-                html.H1('History: from {0} to {1}'.format(str(df.Date[0]), str(df.Date[len(df)])), style={'textAlign': 'center'}),
-                html.H1('Group Members: ' + str(len(df['from'].unique())), style={'textAlign': 'center'}),
-                dcc.Graph(figure = fig_wordcloud),
-                dcc.Graph(figure = fig_pie),
-                dcc.Graph(figure = fig_line), dcc.Graph(figure=fig)]
+
+        cards = html.Div(
+            [
+                jumbotron,
+                dbc.Row(
+                    [
+                        dbc.Col(dbc.Card(
+                                        dbc.CardBody(
+                                            [
+                                                html.H5(labels[0], className="card-title"),
+                                                html.P(
+                                                    'Total Messages: ' + str(values[0]),
+                                                    className="card-text",
+                                                ),
+                                                html.P(
+                                                    'Total words: ' + str(values[0]),
+                                                    className="card-text",
+                                                )
+                                            ]
+                                        )
+                                    )),
+                        dbc.Col(dbc.Card(
+                                        dbc.CardBody(
+                                            [
+                                                html.H5(labels[1], className="card-title"),
+                                                html.P(
+                                                    'Total Messages: ' + str(values[1]),
+                                                    className="card-text",
+                                                ),
+                                                html.P(
+                                                    'Total words: ' + str(values[1]),
+                                                    className="card-text",
+                                                )
+                                            ]
+                                        )
+                                    ))
+                    ],
+                    className="mb-4", style={'textAlign': 'center'}
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(dbc.Card(
+                             [dbc.Row([dbc.Col(dcc.Graph(figure = fig_pie))]),
+                              dbc.Row([dbc.Col(dcc.Graph(figure=fig))]),
+                              dbc.Row([dbc.Col(dcc.Graph(figure = fig_line))])
+                             ]
+                         )),
+
+                    ]
+                ),
+            ]
+        )
+
+        return [html.Div(cards)]
 
 def parse_data(contents, filename):
     content_type, content_string = contents.split(',')
